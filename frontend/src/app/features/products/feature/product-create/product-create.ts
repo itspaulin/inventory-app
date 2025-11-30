a28f39116eb5318a2714f4d46152953760a0bb6a
Paulo Filho <lpbalves7filho@gmail.com>
2025-11-30 20:22:24 -0300

    feat: add products features

---
+++ frontend/src/app/features/products/feature/product-create/product-create.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  imports: [],
+  templateUrl: './product-create.html',
+})
+export class ProductCreate {}
---
+++ frontend/src/app/features/products/feature/product-detail/product-detail.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.html',
+})
+export class ProductDetail {}
