f4421c0052da5614f2dd78400374ec668be1f6c1
Paulo Filho <lpbalves7filho@gmail.com>
2025-12-02 20:46:26 -0300

    feat: add products create

--- frontend/src/app/features/products/feature/product-create/product-create.ts
+++ frontend/src/app/features/products/feature/product-create/product-create.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ProductsService } from '../../domain/services/products';
+import { Product } from '../../domain/models/product.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-create',
   imports: [RouterLink],
   templateUrl: './product-create.html',
 })
-export class ProductCreate {}
+export class ProductCreate implements OnInit {
+  product: Product | null = null;
+
+  constructor(
+    private productsService: ProductsService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.productsService.getById(Number(id)).subscribe({
+        next: (prod) => {
+          this.product = prod;
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
--- frontend/src/app/features/products/feature/product-detail/product-detail.ts
+++ frontend/src/app/features/products/feature/product-detail/product-detail.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../domain/models/product.model';
+import { ProductsService } from '../../domain/services/products';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   imports: [],
   templateUrl: './product-detail.html',
 })
-export class ProductDetail {}
+export class ProductCreate implements OnInit {
+  product: Product | null = null;
+
+  constructor(
+    private productsService: ProductsService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+
+      this.productsService.getById(id).subscribe((prod) => {
+        this.product = prod;
+      });
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
