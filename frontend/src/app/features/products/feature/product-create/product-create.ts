1cc72b471cbad4d6d55e16cbd1812b2ede793600
Paulo Filho <lpbalves7filho@gmail.com>
2025-12-02 21:36:50 -0300

    feat: add products create

--- frontend/src/app/features/products/feature/product-create/product-create.ts
+++ frontend/src/app/features/products/feature/product-create/product-create.ts
@@ -1,39 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductsService } from '../../domain/services/products';
-import { Product } from '../../domain/models/product.model';
-import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-create',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './product-create.html',
 })
 export class ProductCreate implements OnInit {
-  product: Product | null = null;
-
   constructor(
-    private productsService: ProductsService,
-    private location: Location,
-    private route: ActivatedRoute
+    private formBuilder: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+  productForm: FormGroup | null = null;
 
-    if (id) {
-      this.productsService.getById(Number(id)).subscribe({
-        next: (prod) => {
-          this.product = prod;
-        },
-        error: (err) => {
-          console.error(err);
-        },
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      price: ['', [Validators.required, Validators.min(0.01)]],
+      quantity: ['', [Validators.required, Validators.min(0)]],
+      category: [''],
+      sku: [''],
+    });
+  }
+
+  onSubmit() {
+    if (this.productForm?.valid) {
+      console.log('valid');
+
+      const product = this.productForm.value;
+
+      this.productService.create(product).subscribe({
+        next: (res) => console.log('Created product', res),
+        error: (err) => console.error('Erro ao criar produto', err),
+        complete: () => this.router.navigate(['/products']),
       });
+    } else {
+      console.log('Form inválido');
+      this.productForm?.markAsTouched();
     }
   }
 
-  goBack(): void {
-    this.location.back();
+  onCancel(): void {
+    this.router.navigate(['/products']);
   }
 }
